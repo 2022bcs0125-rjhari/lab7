3a4fcb66e7e6f8f5032975a2e086bac95672621f
2022bcs0125-rjhari <rjharicoder@gmail.com>
2026-02-27 12:11:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,18 @@ with open("model.pkl", "rb") as f:
     model = pickle.load(f)
 
 class WineInput(BaseModel):
-    features: list[float]
+    fixed_acidity: float
+    volatile_acidity: float
+    citric_acid: float
+    residual_sugar: float
+    chlorides: float
+    free_sulfur_dioxide: float
+    total_sulfur_dioxide: float
+    density: float
+    pH: float
+    sulphates: float
+    alcohol: float
+    
 @app.get("/",include_in_schema=False)
 def root():
     return RedirectResponse(url="/docs",status_code=301)
@@ -26,3 +37,4 @@ def predict(data: WineInput):
         "roll_no": "2022BCS0125",
         "wine_quality": int(round(prediction))
     }
+
